a83493ad7575849b3bceb716cc8a1974fc72c161
sreeluxx <sreelakshmipallipita@gmail.com>
2026-01-12 23:47:17 +0530

    updated backend api

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://skill-share-lj7s.vercel.app',
         changeOrigin: true,
       },
     },
