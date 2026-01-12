6e6797c7f5d183ed47fbf92f36cae960145ce6fa
sreeluxx <sreelakshmipallipita@gmail.com>
2026-01-13 00:00:55 +0530

    changed vercel config for backend

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
-const app = require('./src/app');
-const connectDB = require('./src/config/db');
+const app = require('../src/app');
+const connectDB = require('../src/config/db');
 
-const PORT = process.env.PORT || 5000;
-connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-}).catch(err => {
-  console.error("DB connection failed:", err);
-});
+let isConnected = false;
+
+async function handler(req, res) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+}
+
+module.exports = handler;
