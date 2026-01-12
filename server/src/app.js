6a637260b20db64ac46972aacb114de6519bf2c4
sreeluxx <sreelakshmipallipita@gmail.com>
2026-01-13 00:11:42 +0530

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -18,6 +18,8 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }));
+
+app.options("*", cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
